176b1300c1baaf7d2f04ccf61a5b46267e04249a
Artёm Skvira <artem.skvira@gmail.com>
2012-03-26 23:10:47 +1100

    First stab at retrieving transactions from each account

--- export.py
+++ export.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
 import re
+import sqlite3
+from collections import namedtuple
+from datetime import date, timedelta
 
 from mechanize import Browser, ControlNotFoundError
 from mechanize import _http
 from pyquery import PyQuery
-from collections import namedtuple
 
 from lib import make_password, get_credentials, write_step, read_step
 
 
 Transaction = namedtuple('Transaction', ['date', 'name', 'desc', 'amount'])
 
+logged_in_url = 'https://ib.nab.com.au/nabib/acctInfo_acctBal.ctl'
 
 def check_url(b, expected_url):
 
     if b.geturl() != expected_url:
-        print('\tExpected URL: ', loggedin_url)
+        print('\tExpected URL: ', expected_url)
         print('\tCurrent URL: ', b.geturl())
         return False
 
@@ -84,7 +87,7 @@ def login():
     b.form.new_control('text', 'hidden', {'name': 'login', 'value': 'Login'})
     b.submit()
 
-    if not check_url(b, 'https://ib.nab.com.au/nabib/acctInfo_acctBal.ctl'):
+    if not check_url(b, logged_in_url):
         print('Error logging in.')
         return None
 
@@ -94,7 +97,7 @@ def login():
 Account = namedtuple('Account', ['acc_id', 'acc_type'])
 
 
-def get_accounts(text):
+def get_account_ids(text):
 
     pq = PyQuery(text)
     account_links = pq.find('#accountBalances_nonprimary_subaccounts a.accountNickname')
@@ -115,7 +118,80 @@ def get_accounts(text):
     return accounts
 
 
+def init_db():
+
+    db = sqlite3.connect('transactions.db')
+    db.execute('''
+        create table if not exists transactions
+              (
+                account_id text,
+
+                date text,
+                details text,
+
+                debit_amount_whole integer,
+                debit_amount_fraction integer,
+
+                credit_amount_whole integer,
+                credit_amount_fraction integer,
+
+                balance_whole integer,
+                balance_fraction integer,
+                balance_sign text
+              )''')
+    return db
+
+
+def is_account_empty(db, account_id):
+
+    cur = db.execute('''
+                     select count(account_id) from transactions
+                     where account_id = ?
+                     ''',
+                     [account_id]
+                    )
+    row = cur.next()
+
+    return row[0] == 0
+
+
+def save_transaction(db,
+                     account_id,
+                     date,
+                     details,
+
+                     debit_amount_whole,
+                     debit_amount_fraction,
+
+                     credit_amount_whole,
+                     credit_amount_fraction,
+
+                     balance_whole,
+                     balance_fraction,
+                     balance_sign):
+
+    db.execute('insert into transactions values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+               (
+                account_id,
+                date,
+                details,
+
+                debit_amount_whole,
+                debit_amount_fraction,
+
+                credit_amount_whole,
+                credit_amount_fraction,
+
+                balance_whole,
+                balance_fraction,
+                balance_sign
+               )
+              )
+
+
 def export():
+
+    db = init_db()
 
     b = login()
     if not b:
@@ -124,20 +200,72 @@ def export():
     response = b.response().read()
     #response = read_step('logged-in.html')
 
-    # Get all account names
-    accounts = get_accounts(response)
+    # Get all account ids and types
+    accounts = get_account_ids(response)
     if not accounts:
         return
 
-    b.select_form(name='submitForm')
-    b.form.set_all_readonly(False)
-    b.form.action = 'https://ib.nab.com.au/nabib/transactionHistoryGetSettings.ctl'
-    b.form['account'] = accounts[0][0]
-    b.form['accountType'] = accounts[0][1]
-    b.submit()
+    for account in accounts:
 
-    response = b.response().read()
-    write_step(accounts[0][0] + '.html', response)
+        account_id = account[0]
+        account_type = account[1]
+
+        if b.geturl() != logged_in_url:
+            print('Current url is %s, need to open accounts page at %s' %
+                  (b.geturl(), logged_in_url))
+            b.open('https://ib.nab.com.au/nabib/acctInfo_acctBal.ctl')
+
+        print('Opening transaction page for account ' + account_id)
+        URL_FORM_ACCOUNT_HISTORY = 'https://ib.nab.com.au/nabib/transactionHistoryGetSettings.ctl'
+        b.select_form(name='submitForm')
+        b.form.set_all_readonly(False)
+        b.form.action = URL_FORM_ACCOUNT_HISTORY
+        b.form['account'] = account_id
+        b.form['accountType'] = account_type
+        b.submit()
+
+        if not check_url(b, URL_FORM_ACCOUNT_HISTORY):
+            return
+
+        # This will render date filtering form
+        URL_TRANS_HISTORY = 'https://ib.nab.com.au/nabib/transactionHistorySelectAccount.ctl'
+        form_url = URL_TRANS_HISTORY + '?filterIndicator=true'
+        b.open(form_url)
+        if not check_url(b, form_url):
+            return
+
+        if is_account_empty(db, account_id):
+
+            print('Account %(acc)s is empty, retrieving transactions from beginning of times' %
+                  {'acc': account_id})
+
+            # It looks like NAB only lets you get transactions from last 560 days
+            # (ancient back-end restriction, I suppose?)
+            # So let's calculate that date then, shall we?
+
+            # periodToDate seems to contain today's date, should be safe to use
+            b.select_form(name='transactionHistoryForm')
+            input_today = b.form['periodToDate'].split('/')
+            today = date(2000 + int(input_today[2]), int(input_today[1]), int(input_today[0]))
+            start_date = today - timedelta(days=560)
+            b.form['periodFromDate'] = start_date.strftime('%d/%m/%y')
+            URL_SUBMIT_HISTORY_FORM = 'https://ib.nab.com.au/nabib/transactionHistoryValidate.ctl'
+            b.form.action = URL_SUBMIT_HISTORY_FORM
+            b.submit()
+
+            response = b.response().read()
+            if not check_url(b, URL_SUBMIT_HISTORY_FORM):
+                return
+
+            write_step(account_id + '.html', response)
+            #if not check_url(b, ''
+
+        else:
+            print('Account %(acc)s has some transactions, so just get the new ones...' %
+                  {'acc': account_id})
+
+        #response = b.response().read()
+        #write_step(account[0] + '.html', response)
 
 
 if __name__ == "__main__":
